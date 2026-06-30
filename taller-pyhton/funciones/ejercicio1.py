58b5fc6a4ee116ff8cd0f78e201fb7b11f5ec339
Joan <Joanstiven883@gmail.com>
2026-06-30 07:15:07 -0500

    Actualizar ejercicio1.py

--- taller-pyhton/funciones/ejercicio1.py
+++ taller-pyhton/funciones/ejercicio1.py
@@ -13,6 +13,10 @@ El programa debe:
 ● Llamar a la función.
 ● Imprimir el saldo mayor."""
 
+saldo1 = float(input("Ingrese el saldo de la cuenta 1: "))
+saldo2 = float(input("Ingrese el saldo de la cuenta 2: "))
+saldo3 = float(input("Ingrese el saldo de la cuenta 3: "))
+
 def saldo_mayor(saldo1, saldo2, saldo3):
     mayor = saldo1
     if saldo2 > mayor:
@@ -20,10 +24,6 @@ def saldo_mayor(saldo1, saldo2, saldo3):
     if saldo3 > mayor:
         mayor = saldo3
     return mayor
-
-saldo1 = float(input("Ingrese el saldo de la cuenta 1: "))
-saldo2 = float(input("Ingrese el saldo de la cuenta 2: "))
-saldo3 = float(input("Ingrese el saldo de la cuenta 3: "))
 
 resultado = saldo_mayor(saldo1, saldo2, saldo3)
 
